1d4e6ea9934b778a86b752b05c8073fea40b7947
UberSnip Developers <developers@ubersnip.com>
2016-03-04 20:21:34 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 
 int main() {
 	KEENIO_QUERYLANGUAGE* keenQL = new KEENIO_QUERYLANGUAGE();
-	keenQL->KEY("EBE009B95941B225CA2A357CEFF401DCB0C2768066DAEB99690CCEFC51FFF65A");
-	keenQL->QueryExec("count media_play(56c6fe8690e4bd30596e08ff) event_collection=media_play timezone=UTC timeframe=this_14_days");
+	keenQL->KEY("<master_key>");
+	keenQL->QueryExec("count media_play(<project_id>) event_collection=media_play timezone=UTC timeframe=this_14_days");
 
 	printf("---QUERY REQUEST---\n%s\n\n\n--RAW REQUEST---\n", (char*)keenQL->ProcessQuery().c_str());
 	
 	
 	KEENIO_CLIENT* kCLIENT = new KEENIO_CLIENT();
-	kCLIENT->kHTTP.reqURL = "https://api.keen.io/3.0/projects/56b6369196773d7eaa5f4bca/events";
+	kCLIENT->kHTTP.reqURL = "https://api.keen.io/3.0/projects/<project_id>/events";
 	kCLIENT->kHTTP.addDefHeaders();
 
 	kCLIENT->kHTTP.addParam("api_key", kCLIENT->kHTTP._masterKey);
