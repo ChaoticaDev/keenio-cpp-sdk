2114ba8d17f0b4dc62b210b69d600c000fad48ab
UberSnip Developers <developers@ubersnip.com>
2016-03-06 07:59:30 -0600

    Update KEENIO_SDK.h

--- KEENIO_SDK.h
+++ KEENIO_SDK.h
@@ -26,9 +26,9 @@ public:
 	map<string, string> _headers;
 	map<string, string> _params;
 
-	string _writeKey = "<write_key>";
-	string _readKey = "<read_key>";
-	string _masterKey = "<master_key>";
+	string _writeKey = "<key>";
+	string _readKey = "<key>";
+	string _masterKey = "<key>";
 
 	void addDefHeaders(void) {
 		this->addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
@@ -298,126 +298,409 @@ public:
 	}
 };
 
+/*
+[
+	{"property_name":"path","operator":"ne","property_value":"/"},
+	{"property_name":"action","operator":"eq","property_value":"/"},
+	{"property_name":"action","operator":"gt","property_value":"/"},
+	{"property_name":"action","operator":"gte","property_value":"/"},
+	{"property_name":"action","operator":"lt","property_value":"/"},
+	{"property_name":"action","operator":"lte","property_value":"/"},
+	{"property_name":"action","operator":"contains","property_value":"/"},
+	{"property_name":"action","operator":"not_contains","property_value":"/"}
+]
+*/
 
-class KEENIO_QUERYLANGUAGE {
-private:
 
-	map<int, string> statements;
-	map<int, string> results;
-	void query_exec() {
+namespace KEENIO_QUERYLANGUAGE {
+	class KEENIO_FILTER {
+	public:
+		string op;
+		string attr;
+		string val;
+	};
 
-	}
-	string api_key;
+	class KEENIO_QUERY {
+	private:
 
-public:
-	string queryURL;
-	string QueryExec(string queryText) {
+		map<int, string> statements;
+		map<int, string> results;
+		void query_exec() {
 
-		int STMT_TYPE;
-
-		if (queryText.find("COUNT") >= 0) {
-			STMT_TYPE = 0;
 		}
-		else if (queryText.find("SUM") >= 0) {
-			STMT_TYPE = 1;
-		}
-		else if (queryText.find("AVERAGE") >= 0) {
-			STMT_TYPE = 2;
-		}
-		else {
-			STMT_TYPE = 666;
-		}
+		string api_key;
 
-		char *str = (char*)queryText.c_str();
-		char * pch;
-		printf("\nSplitting string \"%s\" into tokens:\n", str);
-		pch = strtok(str, " ");
+	public:
+		string queryURL;
+		string QueryExec(string queryText) {
 
-		bool filterMode = false;
-		string sel;
-		string collection;
-		string projectid;
-		string qParams;
+			//COUNT media_play(56c6fe8690e4bd30596e08ff) filters: keenid>0
+			int STMT_TYPE;
 
-		string word;
-
-		while (pch != NULL)
-		{
-			word = pch;
-
-			if (!filterMode) {
-				int setter = word.find("(");
-				int psetter = word.find("=");
-				int fmode = word.find("filters:");
-
-				if (fmode >= 0) {
-					filterMode = true;
-					goto restart_ql_loop;
-				}
-				if (word == "count") {
-					sel = word;
-				}
-				else if (word == "sum") {
-					sel = word;
-				}
-				else if (word == "average") {
-					sel = word;
-				}
-				else if (setter >= 0) {
-					//IS SELECTOR
-
-					int paren1 = word.find("(");
-					collection = word.substr(0, paren1);
-
-					int paren2 = word.find(")") - 1 - paren1;
-					projectid = word.substr(paren1 + 1, paren2);
-					//this->_query = collection;
-
-				}
-				else if (psetter >= 0) {
-					//IS SELECTOR
-					int sep = word.find("=");
-					qParams += "&";
-					qParams += word;
-				}
-			restart_ql_loop:
-				int x = 0;
-
+			if (queryText.find("COUNT") >= 0) {
+				STMT_TYPE = 0;
+			}
+			else if (queryText.find("SUM") >= 0) {
+				STMT_TYPE = 1;
+			}
+			else if (queryText.find("AVERAGE") >= 0) {
+				STMT_TYPE = 2;
 			}
 			else {
-
+				STMT_TYPE = 666;
 			}
 
-			pch = strtok(NULL, " ");
+			char *str = (char*)queryText.c_str();
+			char * pch;
+			printf("\nSplitting string \"%s\" into tokens:\n", str);
+			pch = strtok(str, " ");
+
+			bool filterMode = false;
+			bool concatMode = false;
+			bool initSetterDone = false;
+			string sel;
+			string collection;
+			string projectid;
+			string qParams;
+
+			string word;
+			map<string, KEENIO_FILTER> keenQueryFilters;
+			string keenQueryFilterIndex;
+			string fText = "[";
+
+			while (pch != NULL)
+			{
+				word = pch;
+
+
+				if (!filterMode) {
+					int setter = word.find("(");
+					int psetter = word.find("=");
+					bool fmode = (word == "if");
+
+					if (fmode) {
+						filterMode = true;
+						goto restart_ql_loop;
+					}
+					if (word == "count") {
+						sel = word;
+					}
+					else if (word == "sum") {
+						sel = word;
+					}
+					else if (word == "average") {
+						sel = word;
+					}
+					else if (setter >= 0) {
+						//IS SELECTOR
+
+						int paren1 = word.find("(");
+						collection = word.substr(0, paren1);
+
+						int paren2 = word.find(")") - 1 - paren1;
+						projectid = word.substr(paren1 + 1, paren2);
+						//this->_query = collection;
+
+					}
+					else if (psetter >= 0) {
+						//IS SELECTOR
+						qParams += "&";
+						qParams += word;
+					}
+					int x = 0;
+
+				}
+				else {
+					if (concatMode == false) {
+						int initSetter = word.find("(");
+
+						{
+							int eq = word.find("=");
+							int ne = word.find("<>");
+							int gt = word.find(">");
+							int lt = word.find("<");
+							int gte = word.find(">=");
+							int lte = word.find("<=");
+							int contains = word.find("^=");
+							int not_contains = word.find("!^=");
+							string word1;
+							string wordval;
+							KEENIO_FILTER keenFilter;
+
+							if (eq >= 0 && gte<0 && lte < 0 && contains < 0 && not_contains < 0) {
+								
+								word1 = word.substr(0, eq);
+								wordval = word.substr(eq, word.length() - eq);
+								wordval = word.substr(eq + 1, word.length() - eq - 1);
+
+								if (wordval[0] == '\'') {
+									concatMode = true;
+									wordval = word.substr(eq + 2, word.length() - eq);
+
+									int closeQuote = wordval.find("'");
+									if (closeQuote >= 0) {
+										concatMode = false;
+										wordval = word.substr(eq + 2, word.length() - eq-3);
+									}
+								}
+								keenQueryFilterIndex = word1;
+
+								
+								keenFilter.op = "eq";
+								keenFilter.attr = word1;
+								keenFilter.val = wordval;
+								keenQueryFilters[word1] = keenFilter;
+							}
+							else if (ne >= 0 && gte<0 && lte < 0 && contains < 0 && not_contains < 0) {
+								
+								word1 = word.substr(0, ne);
+								wordval = word.substr(ne, word.length() - ne);
+								wordval = word.substr(ne + 2, word.length() - ne - 3);
+
+								if (wordval[0] == '\'') {
+									concatMode = true;
+									wordval = word.substr(ne + 2, word.length() - ne);
+
+									int closeQuote = wordval.find("'");
+									if (closeQuote >= 0) {
+										concatMode = false;
+										wordval = word.substr(ne + 3, word.length() - ne-4);
+									}
+								}
+								keenQueryFilterIndex = word1;
+
+								
+								keenFilter.op = "ne";
+								keenFilter.attr = word1;
+								keenFilter.val = wordval;
+								keenQueryFilters[word1] = keenFilter;
+							}
+							else if (gt >= 0 && gte < 0) {
+								
+								word1 = word.substr(0, gt);
+								wordval = word.substr(gt, word.length() - gt);
+								wordval = word.substr(gt + 1, word.length() - gt - 1);
+
+								if (wordval[0] == '\'') {
+									concatMode = true;
+									wordval = word.substr(gt + 2, word.length() - gt);
+
+									int closeQuote = wordval.find("'");
+									if (closeQuote >= 0) {
+										concatMode = false;
+										wordval = word.substr(gt + 2, word.length() - gt-3);
+									}
+								}
+								keenQueryFilterIndex = word1;
+
+								
+								keenFilter.op = "gt";
+								keenFilter.attr = word1;
+								keenFilter.val = wordval;
+								keenQueryFilters[word1] = keenFilter;
+							}
+							else if (lt >= 0 && lte < 0) {
+								
+								word1 = word.substr(0, lt);
+								wordval = word.substr(lt, word.length() - lt);
+								wordval = word.substr(lt + 1, word.length() - lt - 1);
+
+								if (wordval[0] == '\'') {
+									concatMode = true;
+									wordval = word.substr(lt + 2, word.length() - lt);
+
+									int closeQuote = wordval.find("'");
+									if (closeQuote >= 0) {
+										concatMode = false;
+										wordval = word.substr(lt + 2, word.length() - lt-3);
+									}
+								}
+								keenQueryFilterIndex = word1;
+
+								
+								keenFilter.op = "lt";
+								keenFilter.attr = word1;
+								keenFilter.val = wordval;
+								keenQueryFilters[word1] = keenFilter;
+							}
+							else if (gte >= 0) {
+								
+								word1 = word.substr(0, gte);
+								wordval = word.substr(gte, word.length() - gte);
+								wordval = word.substr(gte + 2, word.length() - gte - 1);
+
+								if (wordval[0] == '\'') {
+									concatMode = true;
+									wordval = word.substr(gte + 2, word.length() - gte);
+
+									int closeQuote = wordval.find("'");
+									if (closeQuote >= 0) {
+										concatMode = false;
+										wordval = word.substr(gte + 3, word.length() - gte - 4);
+									}
+								}
+								keenQueryFilterIndex = word1;
+
+								
+								keenFilter.op = "gte";
+								keenFilter.attr = word1;
+								keenFilter.val = wordval;
+								keenQueryFilters[word1] = keenFilter;
+							}
+							else if (lte >= 0) {
+								
+								word1 = word.substr(0, lte);
+								wordval = word.substr(lte, word.length() - lte);
+								wordval = word.substr(lte + 2, word.length() - lte - 1);
+
+								if (wordval[0] == '\'') {
+									concatMode = true;
+									wordval = word.substr(lte + 2, word.length() - lte);
+
+									int closeQuote = wordval.find("'");
+									if (closeQuote >= 0) {
+										concatMode = false;
+										wordval = word.substr(lte + 3, word.length() - lte - 4);
+									}
+								}
+								keenQueryFilterIndex = word1;
+
+								
+								keenFilter.op = "lte";
+								keenFilter.attr = word1;
+								keenFilter.val = wordval;
+								keenQueryFilters[word1] = keenFilter;
+							}
+							else if (contains >= 0 && not_contains < 0) {
+								
+								word1 = word.substr(0, contains);
+								wordval = word.substr(contains, word.length() - contains);
+								wordval = word.substr(contains + 1, word.length() - contains - 1);
+
+								if (wordval[0] == '\'') {
+									concatMode = true;
+									wordval = word.substr(contains + 2, word.length() - contains);
+
+									int closeQuote = wordval.find("'");
+									if (closeQuote >= 0) {
+										concatMode = false;
+										wordval = word.substr(contains + 3, word.length() - contains - 4);
+									}
+								}
+								keenQueryFilterIndex = word1;
+
+								
+								keenFilter.op = "contains";
+								keenFilter.attr = word1;
+								keenFilter.val = wordval;
+								keenQueryFilters[word1] = keenFilter;
+							}
+							else if (not_contains >= 0) {
+								
+								word1 = word.substr(0, not_contains);
+								wordval = word.substr(not_contains, word.length() - not_contains);
+								wordval = word.substr(not_contains + 3, word.length() - not_contains - 1);
+
+								if (wordval[0] == '\'') {
+									concatMode = true;
+									wordval = word.substr(not_contains + 3, word.length() - not_contains);
+
+									int closeQuote = wordval.find("'");
+									if (closeQuote >= 0) {
+										concatMode = false;
+										wordval = word.substr(not_contains + 4, word.length() - not_contains - 5);
+									}
+								}
+								keenQueryFilterIndex = word1;
+
+								
+								keenFilter.op = "not_contains";
+								keenFilter.attr = word1;
+								keenFilter.val = wordval;
+								keenQueryFilters[word1] = keenFilter;
+							}
+
+						}
+					}
+					else {
+						int closeQuote = word.find("'");
+						if (closeQuote >= 0) {
+							string tmpWord = word.substr(0, closeQuote);
+							keenQueryFilters[keenQueryFilterIndex].val += " " + tmpWord;
+							concatMode = false;
+
+						}
+						else {
+
+							keenQueryFilters[keenQueryFilterIndex].val += " " + word;
+
+						}
+					}
+				}
+
+			restart_ql_loop:
+				pch = strtok(NULL, " ");
+			}
+
+			this->queryURL = queryText;
+			this->query_exec();
+			string qURL = "https://api.keen.io/3.0/projects/" + projectid;
+			qURL += "/queries/" + sel;
+			qURL += "?api_key=" + this->api_key;
+			qURL += qParams;
+
+			this->queryURL = qURL;
+
+			int kFilIndex = 0;
+
+			std::map<string, KEENIO_FILTER>::iterator kFil;
+			kFil = keenQueryFilters.begin();
+			for(kFil = keenQueryFilters.begin(); kFil != keenQueryFilters.end(); kFil++) {
+				if (kFilIndex == 0) {
+					fText += "{\"property_name\":\"";
+					fText += kFil->second.attr;
+					fText += "\", \"operator\":\"";
+					fText += kFil->second.op;
+					fText += "\", \"property_value\":\"";
+					fText += kFil->second.val;
+					fText += "\"}";
+				}
+				else {
+					fText += ", {\"property_name\":\"";
+					fText += kFil->second.attr;
+					fText += "\", \"operator\":\"";
+					fText += kFil->second.op;
+					fText += "\", \"property_value\":\"";
+					fText += kFil->second.val;
+					fText += "\"}";
+				}
+
+				kFilIndex++;
+			}
+
+			fText += "]";
+
+			qURL += "&filters=" + fText;
+			return qURL;
 		}
 
-		this->queryURL = queryText;
-		this->query_exec();
-		string qURL = "https://api.keen.io/3.0/projects/" + projectid;
-		qURL += "/queries/" + sel;
-		qURL += "?api_key=" + this->api_key;
-		qURL += qParams;
+		void KEY(string key) {
+			this->api_key = key;
+		}
 
-		this->queryURL = qURL;
+		string ProcessQuery() {
+			KEENIO_CLIENT* kCLIENT = new KEENIO_CLIENT();
+			kCLIENT->kHTTP.reqURL = (char*)this->queryURL.c_str();
+			kCLIENT->kHTTP.addDefHeaders();
 
-		return qURL;
-	}
+			kCLIENT->request(kCLIENT->kHTTP);
 
-	void KEY(string key) {
-		this->api_key = key;
-	}
+			return kCLIENT->body;
+		}
 
-	string ProcessQuery() {
-		KEENIO_CLIENT* kCLIENT = new KEENIO_CLIENT();
-		kCLIENT->kHTTP.reqURL = (char*)this->queryURL.c_str();
-		kCLIENT->kHTTP.addDefHeaders();
+		KEENIO_QUERY() {
 
-		kCLIENT->request(kCLIENT->kHTTP);
+		}
+	};
 
-		return kCLIENT->body;
-	}
-
-	KEENIO_QUERYLANGUAGE() {
-
-	}
-};
+}
