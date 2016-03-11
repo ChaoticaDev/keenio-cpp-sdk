76ca218bdf23cb882681f0646b0ee03b4b7733fd
UberSnip Developers <developers@ubersnip.com>
2016-03-10 23:49:56 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,13 +15,13 @@
 int main() {
 	KEENIO_QUERYLANGUAGE::KEENIO_QUERY* keenQL = new KEENIO_QUERYLANGUAGE::KEENIO_QUERY();
 	keenQL->KEY("<key>");
-	keenQL->QueryExec(".export=example_exp.txt >> count pageview(56b6369196773d7eaa5f4bca) event_collection=pageview timezone=UTC timeframe=this_100_days if keen.id>0 && path<>'//'");
+	keenQL->QueryExec(".export=example_exp.txt extraction pageview(56b6369196773d7eaa5f4bca) event_collection=pageview timezone=UTC timeframe=this_100_days if keen.id>0 && path<>'//'");
 
 	printf("---QUERY REQUEST---\n%s", (char*)keenQL->ProcessQuery().c_str());
-
-
+	
+	
 	KEENIO_CLIENT* kCLIENT = new KEENIO_CLIENT();
-	kCLIENT->kHTTP.reqURL = "https://api.keen.io/3.0/projects/56b6369196773d7eaa5f4bca/events";
+	kCLIENT->kHTTP.reqURL = "https://api.keen.io/3.0/projects/PROJ_ID/events";
 	kCLIENT->kHTTP.addDefHeaders();
 
 	kCLIENT->kHTTP.addParam("api_key", kCLIENT->kHTTP._masterKey);
@@ -31,6 +31,6 @@ int main() {
 
 	kCLIENT->request(kCLIENT->kHTTP);
 
-	//printf(kCLIENT->body.c_str());
+	printf(kCLIENT->body.c_str());
 	return 0;
 }
