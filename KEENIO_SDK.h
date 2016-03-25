ea54d72a2d4a0275a371511cc1a60a2a87e703b3
UberSnip Developers <developers@ubersnip.com>
2016-03-24 18:09:39 -0600

    Base64Encoding/Decoding
    Base64 Encode/Decode parameters // Submit data to keenio!

--- KEENIO_SDK.h
+++ KEENIO_SDK.h
@@ -1,3 +1,4 @@
+#pragma once
 #define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
@@ -28,9 +29,9 @@ using namespace std;
 #define KEENIO_HTTP_POST KEENIO_HTTP::METHOD_POST()
 #define KEENIO_HTTP_PUT KEENIO_HTTP::METHOD_PUT()
 
-#define KEENIO_MASTER_KEY kCLIENT->kHTTP.masterKey()
-#define KEENIO_WRITE_KEY kCLIENT->kHTTP.writeKey()
-#define KEENIO_READ_KEY kCLIENT->kHTTP.readKey()
+#define KEENIO_MASTER_KEY masterKey()
+#define KEENIO_WRITE_KEY writeKey()
+#define KEENIO_READ_KEY readKey()
 
 #define KQL_EQUAL "eq" //fully supported
 #define KQL_NOT_EQUAL "ne" //fully supported
@@ -59,7 +60,102 @@ class KEENIO_HTTP {
 	string _writeKey = "<key>";
 	string _readKey = "<key>";
 	string _masterKey = "<key>";
+
+
+
 public:
+
+	static inline bool is_base64(unsigned char c) {
+		return (isalnum(c) || (c == '+') || (c == '/'));
+	}
+
+	std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
+		static const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+		std::string ret;
+		int i = 0;
+		int j = 0;
+		unsigned char char_array_3[3];
+		unsigned char char_array_4[4];
+
+		while (in_len--) {
+			char_array_3[i++] = *(bytes_to_encode++);
+			if (i == 3) {
+				char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+				char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+				char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+				char_array_4[3] = char_array_3[2] & 0x3f;
+
+				for (i = 0; (i < 4); i++)
+					ret += base64_chars[char_array_4[i]];
+				i = 0;
+			}
+		}
+
+		if (i)
+		{
+			for (j = i; j < 3; j++)
+				char_array_3[j] = '\0';
+
+			char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+			char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+			char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+			char_array_4[3] = char_array_3[2] & 0x3f;
+
+			for (j = 0; (j < i + 1); j++)
+				ret += base64_chars[char_array_4[j]];
+
+			while ((i++ < 3))
+				ret += "%3D";
+
+		}
+
+		return ret;
+
+	}
+
+	std::string base64_decode(std::string const& encoded_string) {
+		static const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+		int in_len = encoded_string.size();
+		int i = 0;
+		int j = 0;
+		int in_ = 0;
+		unsigned char char_array_4[4], char_array_3[3];
+		std::string ret;
+
+		while (in_len-- && (encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
+			char_array_4[i++] = encoded_string[in_]; in_++;
+			if (i == 4) {
+				for (i = 0; i < 4; i++)
+					char_array_4[i] = base64_chars.find(char_array_4[i]);
+
+				char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+				char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+				char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+				for (i = 0; (i < 3); i++)
+					ret += char_array_3[i];
+				i = 0;
+			}
+		}
+
+		if (i) {
+			for (j = i; j < 4; j++)
+				char_array_4[j] = 0;
+
+			for (j = 0; j < 4; j++)
+				char_array_4[j] = base64_chars.find(char_array_4[j]);
+
+			char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+			char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+			char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+			for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+		}
+
+		return ret;
+	}
+
+	string bodyRequest;
 	static string METHOD_GET() {
 		return "GET";
 	}
@@ -121,9 +217,22 @@ public:
 		this->_headers[head] = content;
 	}
 
+	string data_param(string prop, string val) {
+
+	}
+
 	//ADD A PARAMETER TO MAP
 	void addParam(string head, string content) {
+
 		this->_params[head] = content;
+
+	}
+
+	//ADD A PARAMETER TO MAP
+	void addDataParam(string head, string content[2]) {
+		int len_s = strlen(("{ \"" + content[0] + "\" : \"" + content[1] + "\" }").c_str());
+		this->_params[head] = this->base64_encode((unsigned const char*)("{ \"" + content[0] + "\" : \"" + content[1] + "\" }").c_str(), len_s);
+
 	}
 
 	//GET ALL HEADERS IN APPROPRIATE HTTP REQUEST FORMAT
@@ -133,6 +242,7 @@ public:
 		//LOOP THROUGH HEADERS MAP -> CONVERT TO HTTP HEADER REQUEST
 		for each (std::pair<string, string> header in this->_headers)
 		{
+			string header_t = header.first + ":" + header.second + "\r\n";
 			plain += header.first + ":" + header.second + "\r\n";
 		}
 
@@ -405,7 +515,7 @@ namespace KEENIO_QUERYLANGUAGE {
 			string getFileContents() {
 				string fileData;
 				string line;
-				ifstream in(this->filePath + "" +this->fileName);
+				ifstream in(this->filePath + "" + this->fileName);
 				while (getline(in, fileData))
 				{
 					line += fileData + '\n';
@@ -480,12 +590,12 @@ namespace KEENIO_QUERYLANGUAGE {
 
 
 				/*
-					LOAD GLOBAL VARIABLES 
-					REPLACE WITH STRING X
+				LOAD GLOBAL VARIABLES
+				REPLACE WITH STRING X
 				*/
 				map<string, string>::iterator VARS_IT;
 				for (VARS_IT = this->VARS.begin(); VARS_IT != this->VARS.end(); VARS_IT++) {
-				
+
 					int pos = word.find(VARS_IT->first);
 					if (pos < 0) {
 						break;
@@ -794,7 +904,7 @@ namespace KEENIO_QUERYLANGUAGE {
 						}
 					}
 					else { // IF CONCAT MODE = TRUE
-						// CONCAT VALUE
+						   // CONCAT VALUE
 						int closeQuote = word.find("'");
 						if (closeQuote >= 0) {
 							string tmpWord = word.substr(0, closeQuote);
@@ -891,9 +1001,9 @@ namespace KEENIO_QUERYLANGUAGE {
 
 		KEENIO_QUERY() {
 
-			/* 
-				SET FILE TO LOAD FOR INITIAL VARIABLE LOADING
-				LOAD GLOBAL VARIABLES
+			/*
+			SET FILE TO LOAD FOR INITIAL VARIABLE LOADING
+			LOAD GLOBAL VARIABLES
 			*/
 
 			FILE_HANDLER fHandle;
@@ -910,7 +1020,7 @@ namespace KEENIO_QUERYLANGUAGE {
 			for (int i = 0; i < Lines.size(); i++) {
 				string line = Lines[i];
 				map<int, string> var_line = STRING_UTILS::splitString(line, "=");
-				
+
 				//IF SPLIT BY `=` SUCCESSFUL
 				if (var_line.size() > 1) {
 
@@ -920,9 +1030,6 @@ namespace KEENIO_QUERYLANGUAGE {
 			}
 
 			/* END LOAD GLOBAL VARIABLES! */
-
-
-
 		}
 	};
 }
