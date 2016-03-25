7d23044058674b2fb00fd337310a9c8752e96855
UberSnip Developers <developers@ubersnip.com>
2016-03-24 18:12:38 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #pragma once
 #include "KEENIO_SDK.h"
 
+// SEND DATA TO KEENIO
+//EVENT = app_event, DATA = 'xLAUNCH' Base64Encoded
+int sendData(){
+	KEENIO_CLIENT* kCLIENT = new KEENIO_CLIENT();
+	kCLIENT->kHTTP.reqURL = "https://api.keen.io/3.0/projects/PROJECT_ID/events";
+	kCLIENT->kHTTP.addDefHeaders();
 
+	kCLIENT->kHTTP.addParam("api_key", "<master_key>");
+
+	string dataParam[2] = { "app_event", "xLAUNCH" };
+	kCLIENT->kHTTP.addDataParam("data", dataParam);
+	kCLIENT->method(KEENIO_HTTP_GET);
+	kCLIENT->request(kCLIENT->kHTTP);
+
+	printf(kCLIENT->body.c_str());
+	
+	return 1;
+}
 
 int main() {
 	KEENIO_QUERYLANGUAGE::KEENIO_QUERY* keenQL = new KEENIO_QUERYLANGUAGE::KEENIO_QUERY();
@@ -15,7 +32,7 @@ int main() {
 	kCLIENT->kHTTP.reqURL = "https://api.keen.io/3.0/projects/PROJECT_ID/events";
 	kCLIENT->kHTTP.addDefHeaders();
 
-	kCLIENT->kHTTP.addParam("api_key", KEENIO_MASTER_KEY);
+	kCLIENT->kHTTP.addParam("api_key", "<read_key>");
 	kCLIENT->kHTTP.addParam("event_collection", "pageview");
 	kCLIENT->kHTTP.addParam("timezone", "UTC");
 	kCLIENT->kHTTP.addParam("timeframe", "this_14_days");
@@ -24,5 +41,8 @@ int main() {
 	kCLIENT->request(kCLIENT->kHTTP);
 
 	printf(kCLIENT->body.c_str());
+	
+	sendData();
+	
 	return 0;
 }
